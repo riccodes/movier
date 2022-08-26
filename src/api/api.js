21e4678335bd6d2f532ad73bc57f1ce4b22ad3b8
ric <roguezoul3@gmail.com>
2022-08-25 19:39:59 -0500

    fix: calling over https again

--- src/api/api.js
+++ src/api/api.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const api_key = "28fa7353824f928bc291c6978cfb86c6"
 const language = "en-US"
 
-const host = "http://api.themoviedb.org/3"
+const host = "https://api.themoviedb.org/3"
 const discoverUrl = "/discover/movie/"
 const genreListUrl = "/genre/movie/list"
 const watchListUrl = id => `/movie/${id}/watch/providers`
 
 //TODO-ADD implement mirage
-const fakeWatchListUrl = "http://localhost:3001/watchProviders"
-const fakeDiscoverUrl = "http://localhost:3001/discover"
+// const fakeWatchListUrl = "http://localhost:3001/watchProviders"
+// const fakeDiscoverUrl = "http://localhost:3001/discover"
 
 // export const getMovie = id => {
 //     axios.get(`${fakeDiscoverUrl}/movie/${id}`, { params: { api_key, language } }).then(response => {
