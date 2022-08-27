b2d2387d17a11624aad30258fcc392406120a8b7
ric <roguezoul3@gmail.com>
2022-08-26 19:38:05 -0500

    add: recommendations

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {Container, FormControl, Grid, InputLabel, MenuItem, Select, Slider, Stack} from "@mui/material";
+import {Alert, Container, FormControl, Grid, InputLabel, MenuItem, Select, Slider, Stack} from "@mui/material";
+import SettingsSuggestTwoToneIcon from '@mui/icons-material/SettingsSuggestTwoTone';
 import MovieCard from "./MovieCard";
-import api from "themoviedb-javascript-library";
+import tmdb from "themoviedb-javascript-library";
 
 function App() {
 
-    api.common.api_key = "28fa7353824f928bc291c6978cfb86c6";
-    api.common.base_uri = "https://api.themoviedb.org/3/";
-    api.common.images_uri = "https://image.tmdb.org/t/p/";
+    tmdb.common.api_key = "28fa7353824f928bc291c6978cfb86c6";
+    tmdb.common.base_uri = "https://api.themoviedb.org/3/";
+    tmdb.common.images_uri = "https://image.tmdb.org/t/p/";
 
     const minYear = 1881
     const currentYear = new Date().getFullYear()
@@ -16,10 +17,15 @@ function App() {
     const [genres, setGenres] = useState([])
     const [selectedGenre, setSelectedGenre] = useState({name: ""})
     const [year, setYear] = useState("")
+    const [alertMessage, setAlertMessage] = useState()
+    const [isMessageDisplay, setIsMessageDisplay] = useState()
     const jsonify = string => JSON.parse(string)
 
     useEffect(() => {
-        api.discover.getMovies({
+        setIsMessageDisplay(false)
+        setAlertMessage(null)
+
+        tmdb.discover.getMovies({
                 watch_region: "US",
                 with_genres: selectedGenre.id,
                 primary_release_year: year,
@@ -31,7 +37,7 @@ function App() {
     }, [year, selectedGenre])
 
     useEffect(() => {
-        api.genres.getMovieList({},
+        tmdb.genres.getMovieList({},
             (response) => { setGenres(jsonify(response).genres) },
             (error) => { console.error(error) }
         )
@@ -41,6 +47,10 @@ function App() {
     const handleGenreSelect = e => {
         const genre = genres.find(genre => genre.name === e.target.value)
         setSelectedGenre(genre)
+    }
+    const setDisplayMessage = (show, message) => {
+        setAlertMessage(message)
+        setIsMessageDisplay(show)
     }
 
     return (
@@ -70,8 +80,15 @@ function App() {
                     max={currentYear}/>
                 {currentYear}
             </Stack>
+            {isMessageDisplay && (
+                <Alert sx={{marginBottom: "32px"}} icon={<SettingsSuggestTwoToneIcon fontSize="inherit" />}>{alertMessage}</Alert>
+            )}
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 20 }}>
-                {movies?.map(movie => <MovieCard movie={movie}/>)}
+                {movies?.map(movie =>
+                    <MovieCard
+                        setDisplayMessage={setDisplayMessage}
+                        setMovies={setMovies}
+                        movie={movie}/>)}
             </Grid>
         </Container>
     )
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,17 @@
 import React, {useEffect, useState} from "react";
-import {Box, Card, CardContent, CardMedia, Chip, Grid, Stack, Typography} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Grid, Stack, Typography} from "@mui/material";
 import StarRateRoundedIcon from "@mui/icons-material/StarRateRounded";
 import {getWatchProviders} from "../api/api";
+import tmdb from "themoviedb-javascript-library";
 
-const getYear = date => {
-    if(date)
-        return date.slice(0, 4)
+const getYear = date => (date ? date.slice(0, 4) : "")
 
-    return ""
-}
-
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie, setDisplayMessage, setMovies}) => {
 
     const [providers, setProviders] = useState()
-    // const [isDisplay, setIsDisplay] = useState(true)
+
+    //TODO-FIX make conditional render of providers
+    // const [isProvidersDisplay, setIsProvidersDisplay] = useState(true)
 
     useEffect(() => {
         getWatchProviders(movie.id).then(providers => {
@@ -21,10 +19,18 @@ const MovieCard = ({movie}) => {
         })
     }, [])
 
-    //TODO-FIX make conditional render of providers work
+    const getRecommendations = () => {
+        tmdb.movies.getRecommendations({id: movie.id},
+            (response) => {
+                setMovies(JSON.parse(response).results)
+                setDisplayMessage(true, `Recommendations based on ${movie.title}`)
+            },
+            (error) => { console.error(error) }
+        )
+    }
+
     const renderProviders = (provs, label) => {
         if (provs) {
-            // setIsDisplay(true)
             return (
                 <Box sx={{marginBottom: "4px"}}>
                     <Typography variant="subtitle2">{label}</Typography>
@@ -40,39 +46,42 @@ const MovieCard = ({movie}) => {
 
     return (
         <Grid item xs={2} sm={4} md={4}>
-        <Card variant="elevation" sx={{margin: "8px"}}>
-            <CardMedia
-                component="img"
-                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                alt={movie.title}
-            />
-            <CardContent>
-                <Stack
-                    direction="row"
-                    justifyContent="space-evenly"
-                    alignItems="flex-start"
-                    spacing={2}
-                >
-                    <Typography gutterBottom variant="h5">
-                        {movie.title} ({getYear(movie.release_date)})
+            <Card variant="elevation" sx={{margin: "8px"}}>
+                <CardMedia
+                    component="img"
+                    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                    alt={movie.title}
+                />
+                <CardContent>
+                    <Stack
+                        direction="row"
+                        justifyContent="space-evenly"
+                        alignItems="flex-start"
+                        spacing={2}
+                    >
+                        <Typography gutterBottom variant="h5">
+                            {movie.title} ({getYear(movie.release_date)})
+                        </Typography>
+                        <Chip
+                            color="info"
+                            label={movie.vote_average}
+                            icon={<StarRateRoundedIcon/>}/>
+                    </Stack>
+                </CardContent>
+                <CardContent>
+                    <Typography variant="caption" color="text.secondary">
+                        {movie.overview}
                     </Typography>
-                    <Chip
-                        color="info"
-                        label={movie.vote_average}
-                        icon={<StarRateRoundedIcon/>}/>
-                </Stack>
-            </CardContent>
-            <CardContent>
-                <Typography variant="caption" color="text.secondary">
-                    {movie.overview}
-                </Typography>
-            </CardContent>
-            <CardContent sx={{background: "#efefef"}}>
-                {renderProviders(providers?.free, "Free")}
-                {renderProviders(providers?.rent, "Rent")}
-                {renderProviders(providers?.buy, "Buy")}
-            </CardContent>
-        </Card>
+                </CardContent>
+                <CardContent sx={{background: "#efefef"}}>
+                    {renderProviders(providers?.free, "Free")}
+                    {renderProviders(providers?.rent, "Rent")}
+                    {renderProviders(providers?.buy, "Buy")}
+                </CardContent>
+                <CardActions>
+                    <Button onClick={getRecommendations} size="small">Recommendations</Button>
+                </CardActions>
+            </Card>
         </Grid>
     )
 }
