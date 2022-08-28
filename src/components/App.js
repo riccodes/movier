0577a95c9526e3f479b0d5c7688a38291b2d7bb0
ric <roguezoul3@gmail.com>
2022-08-28 14:50:11 -0500

    add: rating selection

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import {
     Alert,
     Autocomplete,
-    Box,
     Container,
     FormControl,
     Grid,
+    Rating,
     Slider,
     Stack,
     TextField,
@@ -31,6 +31,7 @@ function App() {
     const [personQuery, setPersonQuery] = useState()
     const [certifications, setCertifications] = useState([])
     const [selectedGenre, setSelectedGenre] = useState({name: ""})
+    const [selectedRating, setSelectedRating] = useState(0)
     const [selectedPerson, setSelectedPerson] = useState({id: ""})
     const [selectedSort, setSelectedSort] = useState(sorts.find(sort => sort.key === "pop.desc"))
     const [selectedCertification, setSelectedCertification] = useState({certification: ""})
@@ -51,11 +52,12 @@ function App() {
                 with_people: selectedPerson.id,
                 primary_release_year: year,
                 sort_by: selectedSort.name,
-                "certification.gte": selectedCertification.certification
+                "certification.gte": selectedCertification.certification,
+                "vote_average.gte": selectedRating
             },
             res => handleSuccess(res, "results", setMovies), handleError
         )
-    }, [selectedCertification, selectedGenre, selectedPerson, selectedSort, year])
+    }, [selectedCertification, selectedGenre, selectedPerson, selectedRating, selectedSort, year])
 
     useEffect(() => {
         tmdb.genres.getMovieList({}, res => handleSuccess(res, "genres", setGenres), handleError)
@@ -86,14 +88,20 @@ function App() {
         if (newValue)
             setSelectedPerson(persons.find(person => person.name === newValue))
     }
+    const handleRatingSelect = e => {
+        if (e) setSelectedRating(e.target.value)
+    }
 
     return (
         <Container sx={{marginTop: "16px"}} maxWidth="xl">
-            <Trailer open={trailerOpen} setTrailerOpen={setTrailerOpen} trailer={trailer} />
+            <Trailer open={trailerOpen} setTrailerOpen={setTrailerOpen} trailer={trailer}/>
             <Typography sx={{marginBottom: "16px"}} variant="h3">
                 MovieR <Typography variant="overline">by riccodes</Typography>
             </Typography>
-            <Box component="form">
+            <Stack
+                direction={{xs: 'column', sm: 'row'}}
+                spacing={{xs: 0, sm: 2, md: 4}}
+                alignItems="center">
                 <Selector
                     handleSelection={handleGenreSelect}
                     label="Genres"
@@ -111,38 +119,41 @@ function App() {
                     label="Rating" items={certifications}
                     target="certification"
                     value={selectedCertification.certification}/>
-            </Box>
-            <Stack direction="row">
-                <FormControl sx={{width: "70%"}}>
-                    <Autocomplete
-                        sx={{marginTop: "16px"}}
-                        disablePortal
-                        id="search-person"
-                        options={generatePersonsOptions(persons)}
-                        onInputChange={handleQueryChange}
-                        onChange={handlePersonSelect}
-                        renderInput={(params) => <TextField {...params} label="Search for by person"/>}
-                    />
-                </FormControl>
-                {/*<Container sx={{width: "30%", marginLeft: "16px", direction: "center"}}>*/}
-                {/*    <Typography>Minimum Rating</Typography>*/}
-                    {/*<Rating onClick={(e) => {*/}
-                    {/*    console.log(e.target.value)*/}
-                    {/*}} name="customized-10" defaultValue={0} max={10}/>*/}
-                {/*</Container>*/}
             </Stack>
-            <Stack spacing={2} direction="row" sx={{mb: 1}} alignItems="center">
+            <FormControl fullWidth>
+                <Autocomplete
+                    fullWidth
+                    sx={{marginTop: "16px"}}
+                    disablePortal
+                    id="search-person"
+                    options={generatePersonsOptions(persons)}
+                    onInputChange={handleQueryChange}
+                    onChange={handlePersonSelect}
+                    renderInput={(params) => <TextField {...params} label="Search for by person"/>}
+                />
+            </FormControl>
+            <Stack direction={{xs: 'column', sm: 'row'}}
+                   spacing={{xs: 1, sm: 2, md: 4}}
+                   sx={{marginBottom: "32px"}}
+                   alignItems="center">
                 {minYear}
                 <Slider
-                    sx={{margin: "32px"}}
                     aria-label="Set year"
-                    defaultValue={currentYear}
+                    defaultValue={0}
                     valueLabelDisplay="on"
                     onChangeCommitted={(_, newValue) => handleYearSelect(newValue)}
                     step={1}
                     min={minYear}
                     max={currentYear}/>
                 {currentYear}
+                <Stack alignItems="center">
+                    <Typography variant="subtitle1">Minimum Rating</Typography>
+                    <Rating
+                        onClick={handleRatingSelect}
+                        name="rating-selector"
+                        defaultValue={0}
+                        max={10}/>
+                </Stack>
             </Stack>
             {isMessageDisplay && (
                 <Alert sx={{marginBottom: "32px"}}
