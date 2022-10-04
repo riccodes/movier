7595fe7774bd320affc88fbe708dd2c8e25aa53a
ric <roguezoul3@gmail.com>
2022-10-03 22:38:44 -0500

    chore: troubleshooting routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-export const watchlistRoute = "/watchlist"
-export const searchRoute = "/search"
-export const recommendationsRoute = "/recommendations"
-export const trendingRoute = "/trending"
+export const watchlistRoute = "watchlist"
+export const searchRoute = "search"
+export const recommendationsRoute = "recommendations"
+export const trendingRoute = "trending"
