5ba5ed155617ad67c11fa72c2a4dcc15617a94b9
ric <roguezoul3@gmail.com>
2022-10-05 20:19:47 -0500

    add: subtitle

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {AppBar, Toolbar, Typography} from "@mui/material";
+import {toTop} from "../util/utils";
+import {recommendationsRoute, searchRoute, trendingRoute, watchlistRoute} from "../routes/routes";
+import {useLocation} from "react-router-dom";
 
 const TopBar = () => {
+
+    const location = useLocation()
+    const [subtitle, setSubtitle] = useState("")
+
+    useEffect(()=> {
+        const {pathname} = location
+        toTop()
+
+        switch (pathname){
+            case "/" + recommendationsRoute:
+                setSubtitle("Recommended")
+                break
+            case "/" + trendingRoute:
+                setSubtitle("Trending")
+                break
+            case "/" + watchlistRoute:
+                setSubtitle("Watchlist")
+                break
+            case "/" + searchRoute:
+                setSubtitle("Search")
+                break
+            default:
+                setSubtitle("")
+        }
+
+    }, [location])
 
     return (
         <AppBar position="static">
             <Toolbar variant="dense">
                 <Typography variant="h6" color="inherit" component="div">
-                    MovieR
+                    MovieR - {subtitle}
                 </Typography>
             </Toolbar>
         </AppBar>
