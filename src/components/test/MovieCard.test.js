220ab4ef47fe698c759ce893a7951a50ea4ae304
Ric N <roguezoul3@gmail.com>
2023-02-11 13:11:47 -0600

    add: some basic tests

---
+++ src/components/test/MovieCard.test.js
@@ -0,0 +1,35 @@
+import {render, screen} from "@testing-library/react";
+import MovieCard from "../MovieCard";
+import {mockMovie} from "../../util/Constants";
+import {FilterProvider} from "../../context/FilterContext";
+import {BrowserRouter} from "react-router-dom";
+import {WatchListProvider} from "../../context/WatchListContext";
+import {CommonProvider} from "../../context/CommonContext";
+import {TMDBProvider} from "../../context/TMDBContext";
+
+test('basic render', async () => {
+    render(
+        <BrowserRouter>
+            <FilterProvider>
+                <CommonProvider>
+                    <WatchListProvider>
+                        <TMDBProvider>
+                            <MovieCard movie={mockMovie}/>
+                        </TMDBProvider>
+                    </WatchListProvider>
+                </CommonProvider>
+            </FilterProvider>
+        </BrowserRouter>
+    )
+
+    const title = await screen.findAllByRole('heading')
+    expect(title).toHaveLength(1)
+
+    const image = await screen.findAllByRole('img')
+    expect(image).toHaveLength(1)
+
+    const sections = await screen.findAllByRole('button')
+    expect(sections[0]).toHaveTextContent("Overview")
+
+})
+
